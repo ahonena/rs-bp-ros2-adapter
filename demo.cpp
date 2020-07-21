a6a21bb2b1a16675213c197636c690bd1ee4f0b4
ahonena <andrei.ahonen@tuni.fi>
2020-07-21 21:28:05 +0300

    Changed to xyz coordinates for rviz2, does not yet work with rviz2

--- demo.cpp
+++ demo.cpp
@@ -16,6 +16,9 @@ http://www.microhowto.info/howto/listen_for_and_receive_udp_datagrams_in_c.html
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
 #include "sensor_msgs/point_cloud2_iterator.hpp"
+#include <cmath>
+//#define PRINT_STUFF
+#define TRANSFORM_TO_XYZ
 
 class MinimalPublisher : public rclcpp::Node
 {
@@ -24,6 +27,7 @@ class MinimalPublisher : public rclcpp::Node
 	: Node("minimal_publisher"), count_(0)
 	{
 		publisher_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("my_rsbp_topic",1);
+		#ifndef TRANSFORM_TO_XYZ
 		message.height = 1;
 		message.width = 384;
 		message.point_step = 6;
@@ -35,17 +39,39 @@ class MinimalPublisher : public rclcpp::Node
 						"reflectivity", 1, sensor_msgs::msg::PointField::UINT8);
 
 		modifier.resize(384);
+		message.header.frame_id = "rsbp";
+		#endif
+		#ifdef TRANSFORM_TO_XYZ
+		message_xyz.height = 1;
+		message_xyz.width = 384;
+		message_xyz.point_step = 25;
+
+		sensor_msgs::PointCloud2Modifier modifier(message_xyz);
+		modifier.setPointCloud2Fields(4, "x", 1, sensor_msgs::msg::PointField::FLOAT64,
+						"y", 1, sensor_msgs::msg::PointField::FLOAT64,
+						"z", 1, sensor_msgs::msg::PointField::FLOAT64,
+						"reflectivity", 1, sensor_msgs::msg::PointField::UINT8);
+
+		modifier.resize(384);
+		message_xyz.header.frame_id = "rsbp_xyz";
+		#endif
 	}
 
 	sensor_msgs::msg::PointCloud2 message;
+	#ifdef TRANSFORM_TO_XYZ
+        sensor_msgs::msg::PointCloud2 message_xyz;
+	#endif
 	std::shared_ptr<sensor_msgs::msg::PointCloud2> message_ptr;
 
 	void publish_pointcloud()
 	{
-		//message.data = "Hello, world! " + std::to_string(count_++);
-		//RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-		//(std::shared_ptr< const MessageT >
 		publisher_->publish(message);
+
+	}
+	void publish_xyz_pointcloud()
+	{
+		publisher_->publish(message_xyz);
+
 	}
 	private:
 
@@ -55,7 +81,6 @@ class MinimalPublisher : public rclcpp::Node
 
 
 
-#define PRINT_STUFF
 
 int main(int argc, char * argv[]){
 	std::cout << "This is a very primitive Robosense MSOP parser and publisher" << std::endl;
@@ -130,17 +155,59 @@ int main(int argc, char * argv[]){
 			int msg_offset = 42;
 			int idx_in_msop = 0;
 			int which_point = 0;
+			uint16_t azimuth_raw = 0;
+			double azimuth = 0.0;
+			uint16_t distance = 0;
+			double elevation = 0;
+			uint8_t reflectivity = 0;
+			double x,y,z = 0;
+			const double elevation_lookuptable[32] = {89.5*(M_PI/180),
+								81.0625*(M_PI/180),
+								78.25*(M_PI/180),
+								72.625*(M_PI/180),
+								67*(M_PI/180),
+								61.375*(M_PI/180),
+								55.75*(M_PI/180),
+								50.125*(M_PI/180),
+								86.6875*(M_PI/180),
+								83.875*(M_PI/180),
+								75.4375*(M_PI/180),
+								69.8125*(M_PI/180),
+								64.1875*(M_PI/180),
+								58.5625*(M_PI/180),
+								52.9375*(M_PI/180),
+								47.3125*(M_PI/180),
+								44.5*(M_PI/180),
+								38.875*(M_PI/180),
+								33.25*(M_PI/180),
+								27.625*(M_PI/180),
+								22*(M_PI/180),
+								16.375*(M_PI/180),
+								10.75*(M_PI/180),
+								5.125*(M_PI/180),
+								41.6875*(M_PI/180),
+								36.0625*(M_PI/180),
+								30.4375*(M_PI/180),
+								24.8125*(M_PI/180),
+								19.1875*(M_PI/180),
+								13.5625*(M_PI/180)};
+
 			for(int n = 0; n < 12; n++){
 
-				uint16_t azimuth = 0;
-				// Verify endianness!
-				memcpy(&azimuth, buffer + msg_offset + 2 + 100*n, 2);
-				uint8_t pointcloud2_datapoint[6];
-				memcpy(pointcloud2_datapoint + 0, &azimuth, 2); // azimuth
 
+
+
+
+				// Verify endianness!
+				memcpy(&azimuth_raw, buffer + msg_offset + 2 + 100*n, 2);
+				uint8_t pointcloud2_datapoint[6];
+				uint8_t pointcloud2_datapoint_xyz[25];
+				memcpy(pointcloud2_datapoint + 0, &azimuth_raw, 2); // azimuth
+				azimuth = double(azimuth_raw)*0.01*(M_PI/180);
  				for(int i = 0; i < 32; i++){
 
 					idx_in_msop = msg_offset + 4 +3*i + 100*n;
+					#ifndef TRANSFORM_TO_XYZ
 					// Order of the data matters both in MSOP and pointcloud2
 					memcpy(pointcloud2_datapoint + 5, buffer + idx_in_msop + 0, 1); // reflectivity
 					memcpy(pointcloud2_datapoint + 3, buffer + idx_in_msop + 1, 2); // distance
@@ -148,11 +215,36 @@ int main(int argc, char * argv[]){
 					for (int j = 0; j < 6; j++){
 						my_publisher.message.data[6*which_point + j] = pointcloud2_datapoint[j];
 					}
+					#else
+					memcpy(&reflectivity, buffer + idx_in_msop + 0, 1);
+					memcpy(&distance, buffer + idx_in_msop + 1, 2);
+					elevation = elevation_lookuptable[i];
+					x = double(distance)*0.01*cos(elevation)*sin(azimuth);
+					y = double(distance)*0.01*cos(elevation)*cos(azimuth);
+					z = double(distance)*0.01*sin(elevation);
+					memcpy(pointcloud2_datapoint_xyz + 0, &x, 8);
+					memcpy(pointcloud2_datapoint_xyz + 8, &y, 8);
+					memcpy(pointcloud2_datapoint_xyz + 16, &z, 8);
+					memcpy(pointcloud2_datapoint_xyz + 24, &reflectivity, 1);
+					// TODO
+					for(int j = 0; j < 25; j++){
+						my_publisher.message_xyz.data[25*which_point + j] = pointcloud2_datapoint_xyz[j];
+					}
+					//my_publisher.message.data[25*which_point + 0] = 
+					//my_publisher.message.data
+					//my_publisher.message.data
+					//my_publisher.message.data
+					#endif
+
 					which_point++;
 				}
 			}
-
+			#ifndef TRANSFORM_TO_XYZ
 			my_publisher.publish_pointcloud();
+			#else
+			my_publisher.publish_xyz_pointcloud();
+			#endif
+			
 		}
 
 
