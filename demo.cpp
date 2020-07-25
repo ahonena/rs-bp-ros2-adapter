dfdc29f3ad283145452178b44f0d6e15ac5d0a2d
ahonena <andrei.ahonen@tuni.fi>
2020-07-25 19:25:23 +0300

    Corrected endianness issues, checked result visually with rviz2, looks weird but good enough for now...

--- demo.cpp
+++ demo.cpp
@@ -20,6 +20,11 @@ http://www.microhowto.info/howto/listen_for_and_receive_udp_datagrams_in_c.html
 //#define PRINT_STUFF
 #define TRANSFORM_TO_XYZ
 
+#ifdef PRINT_STUFF
+const int PRINTMAX = 1;
+#endif
+
+
 class MinimalPublisher : public rclcpp::Node
 {
 	public:
@@ -44,13 +49,13 @@ class MinimalPublisher : public rclcpp::Node
 		#ifdef TRANSFORM_TO_XYZ
 		message_xyz.height = 1;
 		message_xyz.width = 384;
-		message_xyz.point_step = 25;
+		message_xyz.point_step = 13;
 
 		sensor_msgs::PointCloud2Modifier modifier(message_xyz);
-		modifier.setPointCloud2Fields(4, "x", 1, sensor_msgs::msg::PointField::FLOAT64,
-						"y", 1, sensor_msgs::msg::PointField::FLOAT64,
-						"z", 1, sensor_msgs::msg::PointField::FLOAT64,
-						"reflectivity", 1, sensor_msgs::msg::PointField::UINT8);
+		modifier.setPointCloud2Fields(4, "x", 1, sensor_msgs::msg::PointField::FLOAT32,
+						"y", 1, sensor_msgs::msg::PointField::FLOAT32,
+						"z", 1, sensor_msgs::msg::PointField::FLOAT32,
+						"intensity", 1, sensor_msgs::msg::PointField::UINT8);
 
 		modifier.resize(384);
 		message_xyz.header.frame_id = "rsbp_xyz";
@@ -79,6 +84,37 @@ class MinimalPublisher : public rclcpp::Node
 		size_t count_;
 };
 
+
+const double elevation_lookuptable[32] = {89.5*(M_PI/180),
+					81.0625*(M_PI/180),
+					78.25*(M_PI/180),
+					72.625*(M_PI/180),
+					67*(M_PI/180),
+					61.375*(M_PI/180),
+					55.75*(M_PI/180),
+					50.125*(M_PI/180),
+					86.6875*(M_PI/180),
+					83.875*(M_PI/180),
+					75.4375*(M_PI/180),
+					69.8125*(M_PI/180),
+					64.1875*(M_PI/180),
+					58.5625*(M_PI/180),
+					52.9375*(M_PI/180),
+					47.3125*(M_PI/180),
+					44.5*(M_PI/180),
+					38.875*(M_PI/180),
+					33.25*(M_PI/180),
+					27.625*(M_PI/180),
+					22*(M_PI/180),
+					16.375*(M_PI/180),
+					10.75*(M_PI/180),
+					5.125*(M_PI/180),
+					41.6875*(M_PI/180),
+					36.0625*(M_PI/180),
+					30.4375*(M_PI/180),
+					24.8125*(M_PI/180),
+					19.1875*(M_PI/180),
+					13.5625*(M_PI/180)};
 
 
 
@@ -124,8 +160,8 @@ int main(int argc, char * argv[]){
 	//auto rsbp_msg = std::make_shared<sensor_msgs::msg::PointCloud2>();
 
 
-
-
+	int printcounter = 0;
+	bool printed_once = false;
 	// Main loop
 	while(1){
 
@@ -133,6 +169,7 @@ int main(int argc, char * argv[]){
 		struct sockaddr_storage src_addr;
 		socklen_t src_addr_len=sizeof(src_addr);
 		ssize_t count=recvfrom(fd,buffer,sizeof(buffer),0,(struct sockaddr*)&src_addr,&src_addr_len);
+
 		if (count==-1) {
 			fprintf(stderr, "recvfrom error: %s\n",strerror(errno));
 			 return -1;
@@ -148,93 +185,85 @@ int main(int argc, char * argv[]){
 		// MSOP protocol information can be found in robosense manuals
 		else if(count==1248){
 			#ifdef PRINT_STUFF
+			if(printcounter < PRINTMAX){
 			std::cout << std::endl;
 			printf("Robosense MSOP message received, length %d \n", (int) count);
+			}
 			#endif
       
 			int msg_offset = 42;
 			int idx_in_msop = 0;
+			int azimuth_index = 0;
 			int which_point = 0;
+			uint8_t azimuth_raw_bytes[2] = {0,0};
 			uint16_t azimuth_raw = 0;
 			double azimuth = 0.0;
 			uint16_t distance = 0;
+			uint8_t distance_raw_bytes[2] = {0,0};
 			double elevation = 0;
 			uint8_t reflectivity = 0;
-			double x,y,z = 0;
-			const double elevation_lookuptable[32] = {89.5*(M_PI/180),
-								81.0625*(M_PI/180),
-								78.25*(M_PI/180),
-								72.625*(M_PI/180),
-								67*(M_PI/180),
-								61.375*(M_PI/180),
-								55.75*(M_PI/180),
-								50.125*(M_PI/180),
-								86.6875*(M_PI/180),
-								83.875*(M_PI/180),
-								75.4375*(M_PI/180),
-								69.8125*(M_PI/180),
-								64.1875*(M_PI/180),
-								58.5625*(M_PI/180),
-								52.9375*(M_PI/180),
-								47.3125*(M_PI/180),
-								44.5*(M_PI/180),
-								38.875*(M_PI/180),
-								33.25*(M_PI/180),
-								27.625*(M_PI/180),
-								22*(M_PI/180),
-								16.375*(M_PI/180),
-								10.75*(M_PI/180),
-								5.125*(M_PI/180),
-								41.6875*(M_PI/180),
-								36.0625*(M_PI/180),
-								30.4375*(M_PI/180),
-								24.8125*(M_PI/180),
-								19.1875*(M_PI/180),
-								13.5625*(M_PI/180)};
-
+			float x,y,z = 0;
+			#ifdef PRINT_STUFF
+			if(!printed_once){
+				std::cout << "PRINTING FIRST MESSAGE!" << std::endl << std::endl;
+				for(int i = 0; i < 1248; i++){
+					printf("%x ", buffer[i]);
+					
+					
+				}
+			std::cout << std::endl << std::endl;
+			printed_once = true;
+			}
+			#endif
 			for(int n = 0; n < 12; n++){
-
-
-
-
-
 				// Verify endianness!
-				memcpy(&azimuth_raw, buffer + msg_offset + 2 + 100*n, 2);
-				uint8_t pointcloud2_datapoint[6];
-				uint8_t pointcloud2_datapoint_xyz[25];
-				memcpy(pointcloud2_datapoint + 0, &azimuth_raw, 2); // azimuth
+				azimuth_index =  msg_offset + 2 + 100*n;
+				azimuth_raw_bytes[0] = buffer[azimuth_index + 0];
+				azimuth_raw_bytes[1] = buffer[azimuth_index + 1];				
+				//memcpy(&azimuth_raw, buffer + msg_offset + 2 + 100*n, 2);
+
+				uint8_t pointcloud2_datapoint_xyz[13];
+				// WRONG ENDIANNESS:
+				//azimuth_raw = (azimuth_raw_bytes[1] << 8) | (azimuth_raw_bytes[0]);
+				// CORRECT ENDIANNESS:
+				azimuth_raw = (azimuth_raw_bytes[0] << 8) | azimuth_raw_bytes[1];
 				azimuth = double(azimuth_raw)*0.01*(M_PI/180);
+
  				for(int i = 0; i < 32; i++){
 
 					idx_in_msop = msg_offset + 4 +3*i + 100*n;
-					#ifndef TRANSFORM_TO_XYZ
-					// Order of the data matters both in MSOP and pointcloud2
-					memcpy(pointcloud2_datapoint + 5, buffer + idx_in_msop + 0, 1); // reflectivity
-					memcpy(pointcloud2_datapoint + 3, buffer + idx_in_msop + 1, 2); // distance
-					pointcloud2_datapoint[2] = uint8_t(i); // elevation
-					for (int j = 0; j < 6; j++){
-						my_publisher.message.data[6*which_point + j] = pointcloud2_datapoint[j];
-					}
-					#else
-					memcpy(&reflectivity, buffer + idx_in_msop + 0, 1);
-					memcpy(&distance, buffer + idx_in_msop + 1, 2);
+
+					memcpy(&reflectivity, buffer + idx_in_msop + 2, 1);
+					distance_raw_bytes[0] = buffer[idx_in_msop + 0];
+					distance_raw_bytes[1] = buffer[idx_in_msop + 1];
+					distance = (distance_raw_bytes[0] << 8) | (distance_raw_bytes[1]);
+					//memcpy(&distance, buffer + idx_in_msop + 0, 2);
 					elevation = elevation_lookuptable[i];
-					x = double(distance)*0.01*cos(elevation)*sin(azimuth);
-					y = double(distance)*0.01*cos(elevation)*cos(azimuth);
-					z = double(distance)*0.01*sin(elevation);
-					memcpy(pointcloud2_datapoint_xyz + 0, &x, 8);
-					memcpy(pointcloud2_datapoint_xyz + 8, &y, 8);
-					memcpy(pointcloud2_datapoint_xyz + 16, &z, 8);
-					memcpy(pointcloud2_datapoint_xyz + 24, &reflectivity, 1);
-					// TODO
-					for(int j = 0; j < 25; j++){
-						my_publisher.message_xyz.data[25*which_point + j] = pointcloud2_datapoint_xyz[j];
+					x = float(double(distance)*0.005*cos(elevation)*sin(azimuth));
+					y = float(double(distance)*0.005*cos(elevation)*cos(azimuth));
+					z = float(double(distance)*0.005*sin(elevation));
+					#ifdef PRINT_STUFF
+					if(printcounter < PRINTMAX){
+						printf("azimuth_raw: %i\n", azimuth_raw);
+						printf("azimuth: %f\n", azimuth);
+						printf("elevation: %f\n", elevation);
+						printf("distance raw: %x\n", distance);
+						printf("distance calculated: %f\n", 0.005*distance);
+						printf("x: %f\n",x);
+						printf("y: %f\n",y);
+						printf("z: %f\n",z);
+						printcounter++;
 					}
-					//my_publisher.message.data[25*which_point + 0] = 
-					//my_publisher.message.data
-					//my_publisher.message.data
-					//my_publisher.message.data
 					#endif
+					memcpy(pointcloud2_datapoint_xyz + 0, &x, sizeof(float));
+					memcpy(pointcloud2_datapoint_xyz + 4, &y, sizeof(float));
+					memcpy(pointcloud2_datapoint_xyz + 8, &z, sizeof(float));
+					memcpy(pointcloud2_datapoint_xyz + 12, &reflectivity, 1);
+					// TODO
+					for(int j = 0; j < 13; j++){
+						my_publisher.message_xyz.data[13*which_point + j] = pointcloud2_datapoint_xyz[j];
+					}
+
 
 					which_point++;
 				}
